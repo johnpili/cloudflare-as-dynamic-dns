bb7977b9029ac417fd0286cab7f8146c7e267f35
mrprintedwall <JohnPili@psi-incontrol.com>
2019-10-10 13:28:49 +0800

    Added IP change check

--- updateip.sh
+++ updateip.sh
@@ -2,7 +2,9 @@
 
 IP=$(curl -s "https://ipinfo.io/ip")
 echo "THIS IS IP $IP"
-export DYNIP="$IP"
+touch dynip.txt
+OLD_IP=$(cat dynip.txt)
+echo "OLD IP: $OLD_IP"
 
 curlUpdate(){
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/$1/dns_records/$2" \
@@ -11,9 +13,15 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/$1/dns_records/$2" \
 --data "{\"type\":\"A\",\"name\":\"$4\",\"content\":\"$3\",\"proxied\":true}"
 }
 
-curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "johnpili.com"
-curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "www.johnpili.com"
-#curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "domain1.com"
-#curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "www.domain1.com"
-#curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "domain2.com"
-#curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "www.domain2.com"
+if [ "$IP" != "$OLD_IP" ]; then
+    echo "Changing IP"
+    echo $IP > dynip.txt
+
+    curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "johnpili.com"
+    curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "www.johnpili.com"
+    #curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "domain1.com"
+    #curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "www.domain1.com"
+    #curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "domain2.com"
+    #curlUpdate "ZONE_ID" "ID_OF_DNS_RECORD" "$IP" "www.domain2.com"
+
+fi
